18a0c408da148f961c7d5c5bb3771dfb61a5d264
Anurag Kumar Singh <singh.ask@outlook.com>
2023-06-27 22:41:53 +0530

    COMPLETED: 0. most of the things for now

--- main.py
+++ main.py
@@ -7,11 +7,21 @@ def scann_ip(ip):
     arp_request_broadcast = broadcast / arp_request
 
     affirm_recs, = sc.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
-    print("IP\t\t\tMAC ADDRESS")
+
+    clients_rec = []
     for element in affirm_recs:
-        print(element[1].psrc)
-        print(element[1].hwsrc)
-    print("<---------------------------------------------->")
+        client_dict = {"IP": element[1].psrc, "mac": element[1].hwsrc}
+        clients_rec.append(client_dict)
+        return clients_rec
 
 
-scann_ip("192.168.1.11/24")
+def print_result(result_list):
+    print("IP\t\t\tMAC ADDRESS\n<---------------------------------------------->")
+    for client in result_list:
+        print(client["IP"] + "\t\t" + client["mac"])
+
+    return
+
+
+scan_result = scann_ip("192.168.1.11/24")
+print_result(scan_result)
