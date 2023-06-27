fa9c9a2e8d7b6f352dfd89f6b38808dfcddd8ba9
Anurag Kumar Singh <singh.ask@outlook.com>
2023-06-27 18:38:44 +0530

    COMPLETED: 0.algorithm design 1.ARP packet transmission 2.payload reciver 3.result parsing Todo: 0.result table.

--- main.py
+++ main.py
@@ -2,7 +2,16 @@ import scapy.all as sc
 
 
 def scann_ip(ip):
-    sc.arping(ip)
+    arp_request = sc.ARP(pdst=ip)
+    broadcast = sc.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast / arp_request
+
+    affirm_recs, = sc.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    print("IP\t\t\tMAC ADDRESS")
+    for element in affirm_recs:
+        print(element[1].psrc)
+        print(element[1].hwsrc)
+    print("<---------------------------------------------->")
 
 
-scann_ip("192.168.1.11")
+scann_ip("192.168.1.11/24")
