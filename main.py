d12708bda4dadab0ad5cbb7377947b109caa79d2
Anurag Kumar Singh <singh.ask@outlook.com>
2023-07-07 10:51:12 +0530

    Changes by Kumar Priyanshu and Anurag, Fix trial0

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import argparse as option
 
 
 def get_arguments():
+    # Define Variable consistency throughout scope!
     global IP
+    # Object Parser instance for Parsing arguments
     parser = option.ArgumentParser()
+    # parser argument
     parser.add_argument("-t", "--target", dest="ip")
     (IP) = parser.parse_args()
     return IP
@@ -14,8 +17,7 @@ def scann_ip(ip):
     arp_request = sc.ARP(pdst=ip)
     broadcast = sc.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast / arp_request
-
-    affirm_recs, = sc.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    affirm_recs = sc.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
 
     clients_rec = []
     for element in affirm_recs:
@@ -32,6 +34,7 @@ def print_result(result_list):
     return
 
 
+
 target_ip = get_arguments()
-scan_result = scann_ip(target_ip.ip)
+scan_result = scann_ip(target_ip.IP)
 print_result(scan_result)
